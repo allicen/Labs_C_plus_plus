fb0bf95ecf0b8641d7fb5d09f44952d2f351c444
allicen <ellena.hella@yandex.ru>
2023-03-24 15:38:47 +0300

    Мелкие правки.

--- lab2/programm/Triangle.hpp
+++ lab2/programm/Triangle.hpp
@@ -4,23 +4,23 @@
 #include "Point.hpp"
 
 class Triangle {
-	private:
-		Point a_;
-		Point b_;
-		Point c_;
-		
-	public:
-		Point getA();
-		Point getB();
-		Point getC();
-		void setA(Point);
-		void setB(Point);
-		void setC(Point);
-		void print();
-		bool read();
-		void move(double, double, double, double, double, double);
-		double perimeter();
-		Point center();		
+    private:
+        Point a_;
+        Point b_;
+        Point c_;
+        
+    public:
+        Point getA();
+        Point getB();
+        Point getC();
+        void setA(Point);
+        void setB(Point);
+        void setC(Point);
+        void print();
+        bool read();
+        void move(double, double, double, double, double, double);
+        double perimeter();
+        Point center();        
 };
 
 #endif
