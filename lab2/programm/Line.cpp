525723c54f26aed7c3ca3fd7c9b2f2418343ff70
allicen <ellena.hella@yandex.ru>
2023-03-24 15:07:12 +0300

    Мелкие правки.

--- lab2/programm/Line.cpp
+++ lab2/programm/Line.cpp
@@ -7,60 +7,60 @@
 using namespace std;
 
 Point Line::getPointA() {
-	return a_;
+    return a_;
 }
 
 Point Line::getPointB() {
-	return b_;
+    return b_;
 }
 
 void Line::setPointA(Point a) {
-	a_ = a;
+    a_ = a;
 }
 
 void Line::setPointB(Point b) {
-	b_ = b;
+    b_ = b;
 }
 
 void Line::print() {
-	cout << "Point A: ";
-	a_.Print();
-	cout << "Point B: ";
-	b_.Print();
+    cout << "Point A: ";
+    a_.Print();
+    cout << "Point B: ";
+    b_.Print();
 }
 
 void Line::read() {
-	a_.Read("Введите точку A в виде (x,y) ");
-	b_.Read("Введите точку B в виде (x,y) ");
+    a_.Read("Введите точку A в виде (x,y) ");
+    b_.Read("Введите точку B в виде (x,y) ");
 }
 
 void Line::move(double ax, double ay, double bx, double by) {
-	a_.Move(ax, ay);
-	b_.Move(bx, by);
+    a_.Move(ax, ay);
+    b_.Move(bx, by);
 }
 
 double Line::length() {
-	double ax, ay, bx, by, res;
-	ax = a_.GetX();
-	ay = a_.GetX();
-	bx = b_.GetX();
-	by = b_.GetX();
-	
-	res = sqrt((bx - ax) * (bx - ax) + (by - ay) * (by - ay));
-	return res;
+    double ax, ay, bx, by, res;
+    ax = a_.GetX();
+    ay = a_.GetX();
+    bx = b_.GetX();
+    by = b_.GetX();
+    
+    res = sqrt((bx - ax) * (bx - ax) + (by - ay) * (by - ay));
+    return res;
 }
 
 Point Line::center() {
-	double ax, ay, bx, by, res;
-	ax = a_.GetX();
-	ay = a_.GetX();
-	bx = b_.GetX();
-	by = b_.GetX();
-	
-	Point p;
-	p.SetX(ax + bx / 2);
-	p.SetY(ay + by / 2);
-	
-	return p;
+    double ax, ay, bx, by, res;
+    ax = a_.GetX();
+    ay = a_.GetX();
+    bx = b_.GetX();
+    by = b_.GetX();
+    
+    Point p;
+    p.SetX(ax + bx / 2);
+    p.SetY(ay + by / 2);
+    
+    return p;
 }
 
--- lab2/programm/Line.hpp
+++ lab2/programm/Line.hpp
@@ -4,19 +4,19 @@
 #include "Point.hpp"
 
 class Line {
-	private:
-		Point a_;
-		Point b_;
-		
-	public:
-		Point getPointA();
-		Point getPointB();
-		void setPointA(Point);
-		void setPointB(Point);
-		void print();
-		void read();
-		void move(double ax, double ay, double bx, double by);
-		double length();
-		Point center();	
+    private:
+        Point a_;
+        Point b_;
+        
+    public:
+        Point getPointA();
+        Point getPointB();
+        void setPointA(Point);
+        void setPointB(Point);
+        void print();
+        void read();
+        void move(double ax, double ay, double bx, double by);
+        double length();
+        Point center();    
 };
 #endif
