2319250d2e96bac39819c990a90aa8a390c10ea8
allicen <ellena.hella@yandex.ru>
2023-05-08 15:18:48 +0500

    6.2. Примените подход задачи 6.1. для классов Point, Pixel, Line и Triangle.

--- lab6/programm/main.cpp
+++ lab6/programm/main.cpp
@@ -12,6 +12,10 @@ c++ main.cpp -o main -O2 -Wall -Wextra -pedantic -s
 
 #include "Date.hpp"
 #include "Time.hpp"
+#include "Triangle.hpp"
+#include "Pixel.hpp"
+#include "Line.hpp"
+#include "Point.hpp"
 
 #include <iostream>
 
@@ -25,7 +29,19 @@ int main(int argc, char** argv) {
 	
 	Time time;
 	time.read(cin);
-	time.print(cout);	
+	time.print(cout);
+	
+	Point point;
+	point.Read(cin);
+	point.Print(cout);
+	
+	Line line;
+	line.read(cin);
+	line.print(cout);
+	
+	Triangle t;
+	t.read(cin);
+	t.print(cout);
 	
 	return 0;
 }
