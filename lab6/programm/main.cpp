dbbf228a4381f65bc2197fcffc61e5bdf881e006
allicen <ellena.hella@yandex.ru>
2023-05-08 14:47:36 +0500

    Для классов Date и Time измените интерфейс методов Print и Read:

--- lab6/programm/main.cpp
+++ lab6/programm/main.cpp
@@ -10,9 +10,22 @@ UNIX
 c++ main.cpp -o main -O2 -Wall -Wextra -pedantic -s
 */
 
+#include "Date.hpp"
+#include "Time.hpp"
 
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, char** argv) {
+	
+	Date date;
+	date.read(cin);
+	date.print(cout);
+	
+	Time time;
+	time.read(cin);
+	time.print(cout);	
+	
 	return 0;
 }
