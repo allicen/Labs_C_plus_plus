c81547c69edb12ca5998f92d25486198fc170952
allicen <ellena.hella@yandex.ru>
2023-05-09 20:09:53 +0300

    Заготовка для 9.3.

--- lab9/programm/main.cpp
+++ lab9/programm/main.cpp
@@ -19,6 +19,84 @@ c++ main.cpp -o main -O2 -Wall -Wextra -pedantic -s
 
 using namespace std;
 
+class Point
+{
+	public:
+		virtual void Print() const
+		{
+			cout << "Я Точка\n";
+		}
+		
+		virtual ~Point(){}
+};
+
+class Circle : public Point
+{
+	public:
+		void Print() const override
+		{
+			cout << "Я Круг\n";
+		}
+		
+		virtual void Square() const
+		{
+			cout << "Круг имеет площадь\n";
+		}
+
+		~Circle(){}
+};
+
+class Cylinder : public Circle
+{
+	public:
+		void Print() const override
+		{
+			cout << "Я Цилиндр\n";
+		}
+
+		void Square() const override
+		{
+			cout << "Цилиндр имеет площадь\n";
+		}
+
+		virtual void Volume() const
+		{
+			cout << "Цилиндр имеет объем\n";
+		}
+
+		~Cylinder(){}
+};
+
+void test(const Point *p) {
+	cout << "\n------- virtual --------\n";
+	
+	p->Print();
+	
+	cout << "\n------- typeid() -------\n";
+	
+	const type_info &ti = typeid(*p);
+	cout << ti.name() << endl;
+	
+	if( ti == typeid(Circle) )
+	{
+		const Circle *q = static_cast<const Circle *>(p);
+		q->Square();
+	}
+	
+	if( ti == typeid(Cylinder) )
+	{
+		const Cylinder *q = static_cast<const Cylinder *>(p);
+		q->Square();
+		q->Volume();
+	}
+	
+	cout <<"\n--- dynamic_cast<>() ---\n";
+	
+	if(const Circle *q = dynamic_cast<const Circle *>(p); q != nullptr) q->Square();
+	if(const Cylinder *q = dynamic_cast<const Cylinder*>(p); q != nullptr) q->Volume();
+}
+
+
 int main() {
 	// 9.1. 
 	String a;
@@ -67,6 +145,24 @@ int main() {
 		cout << cStack.Pop();
 	
 	cout << endl;
+	
+	
+	
+	// 9.3.
+	Point a; a.Print(); cout<<endl;
+	
+	Circle b; b.Print(); b.Square(); cout<<endl;
+	
+	Cylinder c; c.Print(); c.Square(); c.Volume(); cout<<endl;
+
+	cout <<"------------------------\n";
+	test(&a);
+	cout <<"\n------------------------\n";
+	test(&b);
+	cout <<"\n------------------------\n";
+	test(&c);
+	cout <<"\n------------------------\n";
+	
 	
 	return 0;
 }
