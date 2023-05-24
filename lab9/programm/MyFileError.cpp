4b28f3512823788416134f5aa12ba5bb57a15d5e
allicen <ellena.hella@yandex.ru>
2023-05-25 01:22:21 +0300

    Lab 9

--- lab9/programm/MyFileError.cpp
+++ lab9/programm/MyFileError.cpp
@@ -1,5 +1,5 @@
 #include "MyFileError.hpp"
 
 std::ostream &operator <<(std::ostream &os, const MyFileError &error) {
-	return os << error.what() << ": " << (error.fileName == nullptr ? "null" : error.fileName); 
+    return os << error.what() << ": " << (error.fileName == nullptr ? "null" : error.fileName); 
 }
